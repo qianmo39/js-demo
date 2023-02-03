16ecb058f0832351082b03a0117a751bea033eb3
qianmo39 <qianmo39@outlook.com>
2023-02-03 16:13:07 +0800

    feat: page scroll effects

---
+++ page-scroll-effects/main.js
@@ -0,0 +1,6 @@
+const scroll = document.querySelector(".curve");
+
+window.addEventListener("scroll", () => {
+  let value = 1 + window.scrollY / -500;
+  scroll.style.transform = `scaleY(${value})`;
+});
